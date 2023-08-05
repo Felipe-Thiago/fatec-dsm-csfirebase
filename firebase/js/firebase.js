de581861518b62719323f7b221573948c77351fe
Felipe Thiago <felipethiagodasilva@gmail.com>
2023-08-04 21:13:50 -0300

    Implementação do firebase

--- firebase/js/firebase.js
+++ firebase/js/firebase.js
@@ -49,3 +49,21 @@ function logoutFirebase(){
         alert(`Não foi possível fazer o logout: ${error.message}`)
     })
 }
+
+async function salvaEstabelecimento(estabelecimento){
+    let usuarioAtual = firebase.auth().currentUser
+
+    return await firebase.database().ref('estabelecimentos').push({
+        ...estabelecimento,
+        usuarioInclusao: {
+            uid: usuarioAtual.uid,
+            nome: usuarioAtual.displayName
+        }
+    }) .then(() => {
+        alert('Registro incluído com sucesso!')
+        // limpa o form
+        document.getElementById('formCadastro').reset()
+    }).catch(error => {
+        alert(`Erro ao salvar: ${error.message}`)
+    })
+}
